0159d4e7709b63024d8ee071b81fb88b63537b45
Zypherone <360494+Zypherone@users.noreply.github.com>
2020-06-18 21:19:36 +1000

    updated seeds.js file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,17 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false 
+    }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 let workoutSeed = [
   {
